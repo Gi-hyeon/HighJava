fa13a6b38cd3a41073b594c00b22babf3e36701d
GuGihyeon <ggh4177@gmail.com>
2023-03-10 18:52:55 +0900

    CollectionExam

---
+++ CollectionExam/ConstTest.java
@@ -0,0 +1,11 @@
+package kr.or.ddit.basicenumTest;
+
+public class ConstTest {
+	public final static int RED = 1;
+	public final static int GREEN = 2;
+	public final static int BLUE = 3;
+	
+	public static final int ONE = 1;
+	public static final int TWO = 2;
+	public static final int THREE = 3;
+}
