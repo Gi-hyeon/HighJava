7b79e4c6721bb54e900232d7ac7d9166be49d449
GuGihyeon <ggh4177@gmail.com>
2023-03-16 17:13:14 +0900

    Member Management Program

--- jdbcLearn/JdbcTest06Tea.java
+++ jdbcLearn/JdbcTest06Tea.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import kr.or.ddit.basic.util.DBUtil;
@@ -67,6 +69,9 @@ public class JdbcTest06Tea {
 			case 5:
 				updateMember2();
 				break;
+			case 6:
+				updateMember3();
+				break;
 			case 0:
 				System.out.println("종료...");
 				return;
@@ -77,9 +82,192 @@ public class JdbcTest06Tea {
 		}
 	}
 	
+	private void updateMember3() {
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println("수정할 회원 정보를 입력하세요...");
+		System.out.println("회원ID > ");
+		String id = scanner.nextLine();
+		
+		int count = getMemberCount(id);
+		if (count == 0) {
+			System.out.println(id + "는(은) 없는 회원ID 입니다...");
+			System.out.println("수정 작업을 마칩니다...");
+			return;
+		} 
+		
+		// key값 : 수정할 컬럼명, value값 : 수정할 데이터 값
+		// 수정할 데이터 값이 있을 때만 Map에 추가된다.
+		Map<String,String> dataMap = new HashMap<>();
+		
+		System.out.print("새로운 비밀번호 > ");
+		String newPass = scanner.nextLine().trim(); //""
+		if (!"".equals(newPass)) {
+			dataMap.put("mem_pass", newPass);
+		}
+		
+		System.out.print("새로운 회원이름 > ");
+		String newName = scanner.nextLine().trim();
+		if (!"".equals(newName)) {
+			dataMap.put("mem_name", newName);
+		}
+		
+		System.out.print("새로운 전화번호 > ");
+		String newTel = scanner.nextLine().trim();
+		if (!"".equals(newTel)) {
+			dataMap.put("mem_tel", newTel);
+		}
+		
+		System.out.print("새로운 회원주소 > ");
+		String newAddr = scanner.nextLine().trim();
+		if (!"".equals(newAddr)) {
+			dataMap.put("mem_addr", newAddr);
+		}
+		
+		try {
+			String temp = ""; // SQL문의 set 이후에 수정할 컬럼 설정하는 부분이 저장될 변수
+			// temp 합쳐서 처리하기
+			/*int num = 0;
+				for (String fieldName : dataMap.keySet()) {
+				if (!"".equals(temp)) {
+					temp += ", ";
+				}
+				temp += fieldName + " = '" + dataMap.get(fieldName) + "'";
+			}
+			
+			conn = DBUtil.getConnection();
+			
+			String sql = "update MYMEMBER set " + temp + " where MEM_ID = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, id);*/
+			
+			for (String fieldName : dataMap.keySet()) {
+				if (!"".equals(temp)) {
+					temp += ", ";
+				}
+				temp += fieldName + " = ?";
+			}
+			
+			// temp ? 붙여서 처리하기
+			String sql = "update MYMEMBER set " + temp + " where MEM_ID = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			int num = 1;
+			
+			for (String fieldName : dataMap.keySet()) {
+				pstmt.setString(num++, dataMap.get(fieldName));
+			}
+			pstmt.setString(num, id);
+			
+			
+			int update = pstmt.executeUpdate();
+			
+			if (update > 0) {
+				System.out.println(id + " 회원 정보 수정 완료!!!");
+			} else {
+				System.out.println(id + " 회원 정보 수정 실패...");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			disConnect();
+		}
+	}
+
+	// 회원 정보를 수정하는 메서드 -> 입력한 항목만 수정하기
+//	private void updateMember3() {
+//		// TODO Auto-generated method stub
+//		System.out.println();
+//		System.out.println("수정할 회원 정보를 입력하세요...");
+//		System.out.print("회원ID > ");
+//		String id = scanner.nextLine();
+//		String pass = "";
+//		String name = "";
+//		String tel = "";
+//		String addr = "";
+//		
+//		int count = getMemberCount(id);
+//		if (count == 0) {
+//			System.out.println(id + "는(은) 없는 회원ID 입니다...");
+//			System.out.println("수정 작업을 마칩니다...");
+//			return;
+//		}
+//		
+//		try {
+//			conn = DBUtil.getConnection();
+//			
+//			String sql = "select * from mymember where MEM_ID = ?";
+//			
+//			pstmt = conn.prepareStatement(sql);
+//			pstmt.setString(1, id);
+//			
+//			rs = pstmt.executeQuery();
+//			
+//			while (rs.next()) {
+//				pass = rs.getString("MEM_PASS");
+//				name = rs.getString("MEM_NAME");
+//				tel = rs.getString("MEM_TEL");
+//				addr = rs.getString("MEM_ADDR");
+//			}
+//			
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} finally {
+//			disConnect();
+//		}
+//		
+//		System.out.print("비밀번호 > ");
+//		String newPass = scanner.nextLine();
+//		if (newPass.length() == 0) {newPass = pass;}
+//		
+//		System.out.print("회원이름 > ");
+//		String newName = scanner.nextLine();
+//		if (newName.length() == 0) {newName = name;}
+//		
+//		System.out.print("전화번호 > ");
+//		String newTel = scanner.nextLine();
+//		if (newTel.length() == 0) {newTel = tel;}
+//		
+//		System.out.print("회원주소 > ");
+//		String newAddr = scanner.nextLine();
+//		if (newAddr.length() == 0) {newAddr = addr;}
+//		
+//		try {
+//			conn = DBUtil.getConnection();
+//			String sql = "update MYMEMBER set MEM_PASS = ?, MEM_NAME=?, MEM_TEL=?, MEM_ADDR=? where MEM_ID = ?";
+//			
+//			pstmt = conn.prepareStatement(sql);
+//			pstmt.setString(1, newPass);
+//			pstmt.setString(2, newName);
+//			pstmt.setString(3, newTel);
+//			pstmt.setString(4, newAddr);
+//			pstmt.setString(5, id);
+//			
+//			int update = pstmt.executeUpdate();
+//			
+//			if (update > 0) {
+//				System.out.println(id + " 회원 정보 수정 완료!!!");
+//			} else {
+//				System.out.println(id + " 회원 정보 수정 실패...");
+//			}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} finally {
+//			disConnect();
+//		}
+//		
+//	}
+	
+	// 회원 정보를 수정하는 메서드 -> 원하는 항목을 선택해서 수정하기
 	private void updateMember2() {
 		// TODO Auto-generated method stub
-		String change = "";
+		String updateData = "";
 		int update = 0;
 		String updateFieldTitle = "";
 		
@@ -117,7 +305,7 @@ public class JdbcTest06Tea {
 		
 		System.out.println();
 		System.out.print("새로운 " + updateFieldTitle + " 입력 > ");
-		change = scanner.nextLine();
+		updateData = scanner.nextLine();
 		
 		try {
 			conn = DBUtil.getConnection();
@@ -139,7 +327,7 @@ public class JdbcTest06Tea {
 			
 			pstmt = conn.prepareStatement(sql);
 			
-			pstmt.setString(1, change);
+			pstmt.setString(1, updateData);
 			pstmt.setString(2, id);
 			
 			update = pstmt.executeUpdate();
@@ -377,9 +565,10 @@ public class JdbcTest06Tea {
 		System.out.println("------------------------------");
 		System.out.println("1. 자 료 추 가");
 		System.out.println("2. 자 료 삭 제");
-		System.out.println("3. 자 료 수 정 ");
+		System.out.println("3. 자 료 수 정(전체항목수정)");
 		System.out.println("4. 전 체 자 료 출 력");
-		System.out.println("5. 자 료 수 정2");
+		System.out.println("5. 자 료 수 정2(수정항목선택)");
+		System.out.println("6. 자 료 수 정3(입력항목만수정)");
 		System.out.println("0. 작 업 끝.");
 		System.out.println("------------------------------");
 		System.out.print("작업 선택 > ");
