9c47914ea0748aa0b0fa4af44406cd760f5a5f67
GuGihyeon <ggh4177@gmail.com>
2023-03-31 20:48:49 +0900

    LprodVO

---
+++ webProject/Java Resources/vo/LprodVO.java
@@ -0,0 +1,26 @@
+package kr.or.ddit.vo;
+
+public class LprodVO {
+	private int prod_id;
+	private String prod_gu;
+	private String prod_nm;
+	
+	public int getProd_id() {
+		return prod_id;
+	}
+	public void setProd_id(int prod_id) {
+		this.prod_id = prod_id;
+	}
+	public String getProd_gu() {
+		return prod_gu;
+	}
+	public void setProd_gu(String prod_gu) {
+		this.prod_gu = prod_gu;
+	}
+	public String getProd_nm() {
+		return prod_nm;
+	}
+	public void setProd_nm(String prod_nm) {
+		this.prod_nm = prod_nm;
+	}
+}
