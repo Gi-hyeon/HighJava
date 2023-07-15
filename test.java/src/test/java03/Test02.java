fa673df4250d3bc3c0221d6f3e1e31ee85ca39a6
GuGihyeon <ggh4177@gmail.com>
2023-07-15 09:40:06 +0900

    Test02.java

--- test.java/src/test/java03/Test02.java
+++ test.java/src/test/java03/Test02.java
@@ -1,12 +1,31 @@
 package test.java03;
 
+import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.util.Iterator;
 import java.util.Properties;
 import java.util.Set;
 
+/**
+ * 현재 작성하고 있는 파일을 읽어서 화면에 출력
+ * - FileInputStream
+ * - FileReader -> (o)
+ *  
+ * 
+ */
+
 public class Test02 {
 	public static void main(String[] args) throws UnsupportedEncodingException {
+		
+		// 이 파일 경로 test.java03.Test02
+		// D:\A_TeachingMaterial\webpro\test.java\src\test\java03\Test02.java
+		
+		// 현재 경로 찍기
+		System.out.println(new File("").getAbsolutePath());
+		
+		File file = new File("src\\test\\java03\\Test02.java");
+		System.out.println(file.exists());
+		System.out.println(file.isFile());
 		
 	}
 }
